e5abc5e476ce4683a6259d7246983b195ad4b5b6
michael <mikelkarije@gmail.com>
2019-08-18 18:35:06 +0300

    addition of date and in upper case format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    new Quote(1,'What you do not want done to yourself, do not do to others.','Confucius'),
-    new Quote(2,'Happiness is not something ready-made. It comes from your own actions.','Dalai Lama'),
-    new Quote(3,'Its a helluva start, being able to recognize what makes you happy.','Lucille Ball'),
-    new Quote(4,'It is more fitting for a man to laugh at life than to lament over it.','Seneca'),
-    new Quote(5,'The two enemies of human happiness are pain and boredom.','Arthur Schopenhauer'),
+    new Quote(1,'What you do not want done to yourself, do not do to others.','Confucius',new Date(2019,8,18)),
+    new Quote(2,'Happiness is not something ready-made. It comes from your own actions.','Dalai Lama',new Date(2019,8,18)),
+    new Quote(3,'Its a helluva start, being able to recognize what makes you happy.','Lucille Ball',new Date(2019,8,18)),
+    new Quote(4,'It is more fitting for a man to laugh at life than to lament over it.','Seneca',new Date(2019,8,18)),
+    new Quote(5,'The two enemies of human happiness are pain and boredom.','Arthur Schopenhauer',new Date(2019,8,18)),
  
   ];
 
