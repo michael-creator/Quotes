d8cfb03375710aee7120137b9fd84b4fdcec6282
michael <mikelkarije@gmail.com>
2019-08-18 18:37:49 +0300

    this commit adds date counts in pipes which counts number of days left

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { StrikethroughDirective } from './strikethrough.directive';
     QuoteComponent,
     QuoteDetailComponent,
     StrikethroughDirective,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
