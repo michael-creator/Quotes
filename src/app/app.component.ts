9745441cace00f0059907d86b51596bb45c38ba6
michael <mikelkarije@gmail.com>
2019-08-18 15:47:27 +0300

    add this commits adds quotes to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'What you do not want done to yourself, do not do to others.'
+  quotes:Quote[] = [
+    {id:1, name:'What you do not want done to yourself, do not do to others.'},
+    {id:2,name:'Happiness is not something ready-made. It comes from your own actions.'},
+    {id:3,name:'Its a helluva start, being able to recognize what makes you happy.'},
+    {id:4,name:'It is more fitting for a man to laugh at life than to lament over it.'},
+    {id:5,name:'The two enemies of human happiness are pain and boredom.'},
+ 
+  ];
+
 }
