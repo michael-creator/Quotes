ec154f79fb2276aa4e08a219df753a0a15d9be16
michael <mikelkarije@gmail.com>
2019-08-18 16:26:13 +0300

    addition of detail-components to our work and save necessary changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    {id:1, name:'What you do not want done to yourself, do not do to others.'},
-    {id:2,name:'Happiness is not something ready-made. It comes from your own actions.'},
-    {id:3,name:'Its a helluva start, being able to recognize what makes you happy.'},
-    {id:4,name:'It is more fitting for a man to laugh at life than to lament over it.'},
-    {id:5,name:'The two enemies of human happiness are pain and boredom.'},
+    {id:1, name:'What you do not want done to yourself, do not do to others.',description:''},
+    {id:2,name:'Happiness is not something ready-made. It comes from your own actions.',description:''},
+    {id:3,name:'Its a helluva start, being able to recognize what makes you happy.',description:''},
+    {id:4,name:'It is more fitting for a man to laugh at life than to lament over it.',description:''},
+    {id:5,name:'The two enemies of human happiness are pain and boredom.',description:''},
  
   ];
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quote[] = [
+    {id:1, name:'What you do not want done to yourself, do not do to others.',description:' Confucius'},
+    {id:2,name:'Happiness is not something ready-made. It comes from your own actions.',description:'Dalai Lama'},
+    {id:3,name:'Its a helluva start, being able to recognize what makes you happy.',description:' Lucille Ball'},
+    {id:4,name:'It is more fitting for a man to laugh at life than to lament over it.',description:'Seneca'},
+    {id:5,name:'The two enemies of human happiness are pain and boredom.',description:'Arthur Schopenhauer'},
+ 
+  ];
   constructor() { }
 
   ngOnInit() {
