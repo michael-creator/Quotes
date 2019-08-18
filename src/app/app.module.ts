ca98b0b72f39b581b255741284aac6a641a44e82
michael <mikelkarije@gmail.com>
2019-08-18 19:56:35 +0300

    this commit adds form to the angular app and also adds input where users can add their quote to

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,13 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteDetailComponent,
     StrikethroughDirective,
     DateCountPipe,
+    QuoteFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
